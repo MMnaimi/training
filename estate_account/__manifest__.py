fc8da0a5e2248e2f56fa713231e5a1fb3900dd4e
MMnaimi <mm.naimi@yahoo.com>
2023-09-18 12:53:44 +0430

    create invoice, kanban

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'views/estate_property_view.xml',
     ],
     # only loaded in demonstration mode
 }
--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,16 +1,42 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, Command
 
 class EstateProperty(models.Model):
     _inherit = 'estate.property'
+
+    invoice_id = fields.Many2one('account.move')
 
     def action_sold(self):
 
         invoice = self.env['account.move'].create({
             'partner_id': self.buyer.id,
             'move_type': 'out_invoice',
-
+            
+            'invoice_line_ids': [
+                Command.create({
+                    'name': f'6% of the selling price',
+                    'quantity': 1,
+                    'price_unit': self.selling_price * 0.06
+                }),
+                Command.create({
+                    'name': 'administrative fees',
+                    'quantity': 1,
+                    'price_unit': 100
+                })
+            ]
         })
+        self.invoice_id = invoice.id
 
         return super(EstateProperty, self).action_sold()
+    
+
+    def open_invoice(self):
+        for rec in self:
+            return {
+                'name': 'Invoice',
+                'type': 'ir.actions.act_window',
+                'res_model': 'account.move',
+                'view_mode': 'form',
+                'res_id': rec.invoice_id.id,
+            }
